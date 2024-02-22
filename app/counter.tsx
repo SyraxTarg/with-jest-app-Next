2c4c86a43090fe95b34c3153c0fb54e0e6b9926a
SyraxTarg <maimiti.saint-marc@efrei.net>
2024-02-22 15:26:11 +0100

    test

--- app/counter.tsx
+++ app/counter.tsx
@@ -10,7 +10,6 @@ export default function Counter() {
       <button type="button" data-testid="button-add" onClick={() => setCount(count + 1)}>
         +
       </button>
-      
       <button type="button" data-testid="button-substract" onClick={() => setCount(count - 1)}>
         -
       </button>
